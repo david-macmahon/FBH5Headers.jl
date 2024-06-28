872c06a847dea44692dfdef91c376cbee1093db5
David MacMahon <davidm@astro.berkeley.edu>
2024-06-28 15:41:59 -0700

    Streamline the code in multiple ways
    Connect to remote workers over 1 GbE but bind to their 10 GbE
    interfaces.  Use `pmap` rather than `RemoteChannel`.  Create a DataFrame
    of the results.  Use ProgressBar when processing files.

--- src/FBH5Headers.jl
+++ src/FBH5Headers.jl
@@ -1,19 +1,19 @@
-using CSV, Distributed, Dates
+using CSV, Distributed, Dates, Sockets, ProgressBars, DataFrames
 import Pkg
 
 @info "starting $(now())"
 
 ##
 
-# Add worker procs
-ws = addprocs(collect(("blpc$i", :auto) for i in 0:3);
+@info "adding worker processes"
+@time ws = addprocs([("blpc$i $(getaddrinfo("blpc$i.tenge.pvt"))", :auto) for i in 0:3];
     exeflags="--project=$(Pkg.project().path)"
 )
 @info "added $(nworkers()) worker processes"
 
 ##
 
-@info "defining functions everywhere"
+@info "defining h5header function everywhere"
 
 @everywhere using HDF5, Blio
 
@@ -30,19 +30,6 @@ ws = addprocs(collect(("blpc$i", :auto) for i in 0:3);
     # Add h5name field to header
     fbh[:h5name] = h5name
     fbh
-end
-
-@everywhere function h5headers(h5name_channel, fbh_channel)
-    # Loop through input files
-    for h5name in h5name_channel
-        if isempty(h5name)
-            put!(fbh_channel, Filterbank.Header())
-            break
-        end
-        put!(fbh_channel, h5header(h5name))
-    end
-    @debug "done"
-    nothing
 end
 
 ##
@@ -62,64 +49,38 @@ end
 
 ##
 
-# Create inter-proc channels and start worker procs
-
-@info "creating remote channels to communicate with worker processes"
-# Main process will put! h5names into h5name_channel.  Worker prcesses will take
-# h5names from h5name_channel, read the header, and push the header to the
-# fbh_channel ("fbh" for "Filterbank,Header").
-h5name_channel = RemoteChannel(()->Channel{String}(Inf))
-fbh_channel = RemoteChannel(()->Channel{Filterbank.Header}(Inf))
-
-@info "spawning one task on each worker process"
-futures = [@spawnat w h5headers(h5name_channel, fbh_channel) for w in ws]
-
-##
-
 @info "getting list of h5names"
 @time h5names = geth5names("/home/jasjeev/c_band_data_0002.out")
-#@time h5names = geth5names("/home/davidm/foo.out")
-@info "got $(length(h5names)) file names"
-
-@info "adding h5names to h5name_channel"
-for h5name in h5names
-    put!(h5name_channel, h5name)
-end
-
-# Put one empty string per worker to signal end of input
-for i = 1:nworkers()
-    put!(h5name_channel, "")
-end
+@info "got $(length(h5names)) h5names"
 
 ##
 
-@info "waiting for workers to finish"
-# Wait for the workers to finish.  No need to fetch since the workers don't
-# return anything (other than through the fbh_channel)
-
-waitforall(futures) = @time foreach(future->wait(future), futures)
-
-#@profview waitforall(futures)
-waitforall(futures)
+@info "parallel map h5names to headers using workers"
+@time fbhs = pmap(h5header, ProgressBar(h5names); batch_size=50)
+@info "got $(length(fbhs)) headers"
 
 ##
 
-remaining = nworkers()
-@info "collecting results from $remaining workers..."
+@info "creating DataFrame"
+@time begin
+    fbhsgood = filter(fbh->length(fbh)>1, fbhs)
+    df = DataFrame(fbhsgood)
+    # Sort column names, but put :h5name at the end
+    select!(df, sort(names(df, Not(:h5name))), :h5name)
+    # Sort rows by tstart then fch1
+    sort!(df, [:tstart, :fch1])
+end;
 
-fbhs = Filterbank.Header[]
-while remaining > 0
-    fbh = take!(fbh_channel)
-    if isempty(fbh)
-        global remaining -= 1
-    else
-        push!(fbhs, fbh)
-    end
-end
+##
 
 @info "removing worker processes"
 rmprocs(ws)
 
-@info "got $(length(fbhs)) headers"
+# Any errors?
+fbhsbad = filter(fbh->length(fbh)<=1, fbhs)
+if length(fbhsbad) > 0
+    @info "$(length(fbhsbad)) files had errors"
+    println.(getindex.(fbhsbad, :h5name))
+end
 
 @info "done $(now())"
